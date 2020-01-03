27c4031c0230c3b550d38c66b0b9f8c1da28fb6d
Jiae Lee <50904635+jiaelee77@users.noreply.github.com>
2020-01-03 09:49:55 +0900

    Add files via upload

---
+++ RSA.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<math.h>
+
+void encryption(){
+
+    //c=pow(m,2) %n;
+}
+
+void decryption(){
+    //m = c^d mod n
+}
+
+int primeNum(){
+
+    double num;
+
+    return num;
+}
+
+int gcd(double e, double p){
+
+    double coprime;
+
+    return coprime;
+}
+
+int main(){
+
+    //#1. Pick two prime number (randomly).
+    double p=primeNum();
+    double q;
+    while(p!=q){ q=primeNum();}
+
+    //#2. get modulus n
+    double n=p*q;
+
+    //#3. calculate P (toitient)
+    double P=(p-1)*(q-1);
+
+    //#4. find the public key 1<e<P && corpime to P .i.e. gcd(e,P)=1
+    double e;
+    
+    //#5. choose the private key
+    double d;
+    //d=(1+k*p)/e;
+
+    //#6. encrypt message m to cyper text c
+
+
+    //#7. dencrypt c to m
+
+    return 0;
+}
